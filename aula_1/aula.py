04b62adaa61dc5903d03295cc91b3f891ad5c6e3
unknown <leonardo.bang@dominio.ufms.br>
2023-03-20 19:01:12 -0400

    Comit das pastas e arquivos que faltam

---
+++ aula_1/aula.py
@@ -0,0 +1,27 @@
+x = 12
+print(x)
+
+y = 44
+x = 55
+x = x + y
+
+print(x)
+
+valor = 12
+y = 5
+valor = (valor/5)
+
+salario = 800.55
+comissso = 10
+salario = salario+comissso*salario/100
+print(salario)
+
+#-------------------------------------------
+
+num1 = float(input("Digite uma numero:"))
+num2 = float(input("Digite outro numero:"))
+
+result = num1 + num2
+
+print(f"Seu resultado é: {result:.2f}")
+
---
+++ aula_1/print.py
@@ -0,0 +1,15 @@
+print("ola","Mundo", sep="-")
+
+print("Hello", end=" ")
+print("world")
+
+#------------------------------
+frase = "Bom dia galera, como vocês estão"
+
+print(frase + frase, sep="\n")
+
+print("idade = %d, peso = .3f" %(12.554, 67.334))
+
+n = input("Digite um numero: ")
+
+print(f"{n:.2f}")
